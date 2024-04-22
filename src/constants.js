6a804323c90b118b3b2adbd426c1e79e9d8e3b1e
Mahi Srivastava <mahidlw@gmail.com>
2024-04-22 10:44:15 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in Python, Machine Learning and GenAI these days.
+    I've made quite a few projects in them. I also like Web Scraping and using different CyberSec tools.
+    Here is my <a href="https://github.com/mahi1722" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it as for a person like me, who left his hometown to learn and hustle, to make a life of his own, it does really counts a lot.`,
+  "sofa-table": `That's my sofa. I like to relax here, watch YouTube and write articles on Generative AI and other technical stuff. 
+  Take a look at my <a href="https://www.linkedin.com/in/mahi-srivastava17/" target="_blank">Linkedin</a>! (Would love to connect with you.)`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  bed: `In the cradle of my bed, where dreams weave their thread,
+  Ideas dance like whispers softly spread.
+  Amidst the night's embrace, they linger unsaid,
+  A symphony of thoughts before slumber's stead.`,
+  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1k5SWfQySiM1IEun-Ko0kpPuhXYE_XLTZ/view?usp=sharing" target="_blank">Check it out?</a>
+  Contact me at mahidlw@gmail.com if you have any interesting job opportunities!`,
+  projects: `I did several projects on Machine Learning, Deep Learning and obviously Generative AI. Take a look at my <a href="https://youtube.com/" target="_blank">Projects</a>.`,
+  library: `There are a lot of fiction books and romance novels on my shelves. Authors like Durjoy Dutta and Agatha Christie are my prime reads.`,
+  exit: `If you want to exit Mahi's portfolio, just close the tab.`,
+};
